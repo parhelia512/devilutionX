2c1a364da4a7d8bf39003f1b97ee5d4a837cee5b
NiteKat <dcm254@gmail.com>
2026-07-30 11:14:09 -0400

    Add New Quest State for Mushroom Quest (#8475)

--- Source/towners.cpp
+++ Source/towners.cpp
@@ -367,8 +367,9 @@ void TalkToWitch(Player &player, Towner & /*witch*/)
 				}
 			}
 			if (Quests[Q_MUSHROOM]._qvar1 >= QS_MUSHGIVEN) {
-				if (HasInventoryItemWithId(player, IDI_BRAIN)) {
+				if (HasInventoryItemWithId(player, IDI_BRAIN) && Quests[Q_MUSHROOM]._qvar2 != TEXT_MUSH11) {
 					Quests[Q_MUSHROOM]._qmsg = TEXT_MUSH11;
+					Quests[Q_MUSHROOM]._qvar2 = TEXT_MUSH11;
 					NetSendCmdQuest(true, Quests[Q_MUSHROOM]);
 					InitQTextMsg(TEXT_MUSH11);
 					return;
