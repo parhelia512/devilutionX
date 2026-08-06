aff9a4a9c886b26766801d705923150b4b195b92
MoonBnuuy <myraloveless@gmail.com>
2026-08-05 21:28:14 -0400

    Restore AmigaOS build and runtime support (#8660)

--- CMake/platforms/amiga.cmake
+++ CMake/platforms/amiga.cmake
@@ -1,3 +1,5 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/threads-stub")
+
 set(BUILD_TESTING OFF)
 set(ASAN OFF)
 set(UBSAN OFF)
@@ -8,9 +10,11 @@ set(SDL1_VIDEO_MODE_BPP 8)
 set(DEVILUTIONX_SYSTEM_BZIP2 OFF)
 set(DEVILUTIONX_SYSTEM_ZLIB OFF)
 
-# Lower the optimization level to O2 because there are issues with O3.
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2")
-set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O2")
+# Lower the optimization level to O1 because m68k-amigaos GCC miscompiles at
+# -O2 and above. See also the AMIGA branch in Source/CMakeLists.txt, which keeps
+# the rendering code from being bumped back up to -O2.
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O1")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O1")
 
 # `fseeko` fails to link on Amiga.
 add_definitions(-Dfseeko=fseek)
