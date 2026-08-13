a7014af1c828a7bbb5e604efc5c7026fcc0b021d
StephenCWills <staphen@gmail.com>
2026-08-13 18:29:10 -0400

    [Amiga] Switch from the archived repo to the AmigaOS3 branch of the active repo

--- Packaging/amiga/prep.sh
+++ Packaging/amiga/prep.sh
@@ -3,27 +3,16 @@
 # exit when any command fails
 set -euo pipefail
 
-#set compiler params
-export TARGET='m68k-amigaos'
-export SYSROOT=/opt/$TARGET
-export M68K_CPU=68040
-export M68K_FPU=hard
-export M68K_CPU_FPU="-m${M68K_CPU} -m${M68K_FPU}-float"
-export M68K_COMMON="-s -ffast-math -fomit-frame-pointer -fbbb=-"
-export M68K_CFLAGS="${M68K_CPU_FPU} ${M68K_COMMON}"
-export M68K_CXXFLAGS="${M68K_CPU_FPU} ${M68K_COMMON}"
-
 PARALLELISM="$(getconf _NPROCESSORS_ONLN)"
 
 mkdir -p deps
-mkdir -p ${SYSROOT}/usr/lib
-mkdir -p ${SYSROOT}/usr/include
 cd deps
 
 # SDL1.2
-wget https://github.com/AmigaPorts/libSDL12/archive/master.tar.gz -O SDL-1.2.tar.gz
+wget https://github.com/AmigaPorts/SDL-1.2/archive/AmigaOS3.tar.gz -O SDL-1.2.tar.gz
 tar -xvf SDL-1.2.tar.gz
-cd libSDL12-master
+cd SDL-1.2-AmigaOS3
+
 # GCC 14+ promotes several legacy-C conformance warnings to hard errors by
 # default (implicit-function-declaration, implicit-int, int-conversion,
 # incompatible-pointer-types, return-mismatch, etc). This codebase predates
@@ -37,11 +26,23 @@ cd libSDL12-master
 # change; Make variable assignments apply regardless of where they appear.
 GCC_MAJOR_VERSION="$(m68k-amigaos-gcc -dumpversion | cut -d. -f1)"
 if [ "$GCC_MAJOR_VERSION" -ge 14 ]; then
-	printf '\nGCCFLAGS += -Wno-error=implicit-function-declaration -Wno-error=implicit-int -Wno-error=int-conversion -Wno-error=incompatible-pointer-types -Wno-error=return-mismatch -Wno-error=return-type -Wno-error=pointer-sign -Wno-error=pointer-to-int-cast -Wno-error=int-to-pointer-cast\n' >> Makefile
+    export CFLAGS="-Wno-error=implicit-function-declaration -Wno-error=implicit-int -Wno-error=int-conversion -Wno-error=incompatible-pointer-types -Wno-error=return-mismatch -Wno-error=return-type -Wno-error=pointer-sign -Wno-error=pointer-to-int-cast -Wno-error=int-to-pointer-cast"
 fi
-make PREFX=${SYSROOT} PREF=${SYSROOT} -j"$PARALLELISM"
-mkdir -p ${SYSROOT}/usr/lib
-mkdir -p ${SYSROOT}/usr/include
-cp -fvr libSDL.a ${SYSROOT}/usr/lib/
-cp -fvr include/* ${SYSROOT}/usr/include/
+
+cmake \
+    -S . \
+    -B build \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DM68K_COMMON="-s -ffast-math -fomit-frame-pointer -fbbb=-" \
+    -DM68K_CPU=68040 \
+    -DM68K_FPU=hard \
+    -DAPOLLOBLIT=OFF \
+    -DGIT_TAG=1.2.15
+
+cmake --build build -j"$PARALLELISM"
+
+# cmake --install is broken on amigadev/crosstools
+cmake -P build/cmake_install.cmake
+
 cd ..
